0dacc1377e195e9a25bfb48019bea5fa50ad6719
Jack Agosthazi <jack.agosthazi@gmail.com>
2017-09-18 22:42:57 -0700

    initial checkin

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+/*global angular */
+/*jshint unused:false */
+'use strict';
+
+/**
+ * The main TodoMVC app module
+ *
+ * @type {angular.Module}
+ */
+var penny = angular.module('penny', ['firebase', 'ui.bootstrap']);

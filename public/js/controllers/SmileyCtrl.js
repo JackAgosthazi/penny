67a3dc916a0bc99cc853fb2c95836b63926bba2b
Jack Agosthazi <jack.agosthazi@gmail.com>
2017-09-27 16:08:56 -0700

    removing negative emotions ;)

--- public/js/controllers/SmileyCtrl.js
+++ public/js/controllers/SmileyCtrl.js
@@ -1,73 +1,93 @@
 'use strict';
 
 penny.controller('SmileyCtrl', function MainCtrl($scope, $rootScope, $location, $firebaseArray, $state) {
-	
-	var eventsUrl = `https://penny-for-your-thought.firebaseio.com/users/${currentUid}/events`;
-	var eventsRef = new Firebase(eventsUrl);
-	var eventsArray = $firebaseArray(eventsRef);
 
-	var emotionsUrl = `https://penny-for-your-thought.firebaseio.com/users/${currentUid}/emotions`;
-	var emotionsRef = new Firebase(emotionsUrl);
-	var emotionsArray = $firebaseArray(emotionsRef);
-	
-	const standardEmotions = ['Angry', 'Sad', 'Jealous', 'Frustrated'];
-	
-	$scope.tags = _.cloneDeep(standardEmotions);
+  var eventsUrl = `https://penny-for-your-thought.firebaseio.com/users/${currentUid}/events`;
+  var eventsRef = new Firebase(eventsUrl);
+  var eventsArray = $firebaseArray(eventsRef);
 
-	// add custom emotions once they loaded
-	emotionsArray.$loaded()
-		.then(function(data) {
-			$scope.tags = _.cloneDeep(standardEmotions).concat(_.map(data, 'name'))
-		});
-	
-	$scope.smileyClicked = function(type){
-		
-		if(type == 'penny-happy'){
-			logEvent('penny-happy');
-		}else if(type == 'penny-sad'){
-			// open sad options
-			$state.go('negative');
-		}else{
-			logEvent(type);
-		}
-	};
-	
-	$scope.addTag = function(){
-		var name = prompt("What should the new emotion be?", "Bummed out");
-		var emotion = {
-			name: name,
-			type: 'negative'
-		};
-		
-		if (name != null && name != "") {
-			emotionsArray.$add(emotion).then(function() {
-				$rootScope.alerts.push({ type: 'success', msg: `Added new emotion: ${name}`});
+  var emotionsUrl = `https://penny-for-your-thought.firebaseio.com/users/${currentUid}/emotions`;
+  var emotionsRef = new Firebase(emotionsUrl);
+  var emotionsArray = $firebaseArray(emotionsRef);
 
-				$scope.tags = _.cloneDeep(standardEmotions).concat(_.map(emotionsArray, 'name'));
-				
-				// TODO: reload tags
-			});
-		}
-	};
-	
-	function logEvent(type){
-		console.log('log type: ', type);
-		
-		var event = {
-			type,
-			time: new Date().getTime()
-		};
+  $scope.tagOnLongPress = function (tag, index) {
+    var isRemove = confirm('Would you like to remove this emotion from the list?');
 
-		eventsArray.$add(event).then(function() {
-			
-			// make standard type human-readable
-			if(type == 'penny-happy'){
-				type = 'Happy';
-			}
-			
-			$rootScope.alerts.push({ type: 'success', msg: `Logged ${type} emotion`});
-			$state.go('smiley');
-		});
-	}
-	
+    if (isRemove) {
+      emotionsArray.$remove(tag).then(function () {
+        $rootScope.alerts.push({type: 'success', msg: `Removed emotion: ${tag.name}`});
+      });
+    }
+  };
+
+  // add custom emotions once they loaded
+  emotionsArray.$loaded()
+    .then(function (data) {
+      if (data.length == 0) {
+        createStandardEmotions();
+      }
+
+      $scope.tags = data;
+    });
+
+  $scope.smileyClicked = function (type) {
+
+    if (type == 'penny-happy') {
+      logEvent('penny-happy');
+    } else if (type == 'penny-sad') {
+      // open sad options
+      $state.go('negative');
+    } else {
+      logEvent(type);
+    }
+  };
+
+  $scope.addTag = function () {
+    var name = prompt("What should the new emotion be?", "Bummed out");
+    var emotion = {
+      name: name,
+      type: 'negative'
+    };
+
+    if (name != null && name != "") {
+      emotionsArray.$add(emotion).then(function () {
+        $rootScope.alerts.push({type: 'success', msg: `Added new emotion: ${name}`});
+
+        $scope.tags = emotionsArray;
+
+        // TODO: reload tags
+      });
+    }
+  };
+
+  function logEvent(type) {
+    var event = {
+      type,
+      time: new Date().getTime()
+    };
+
+    eventsArray.$add(event).then(function () {
+
+      // make standard type human-readable
+      if (type == 'penny-happy') {
+        type = 'Happy';
+      }
+
+      $rootScope.alerts.push({type: 'success', msg: `Logged ${type} emotion`});
+      $state.go('smiley');
+    });
+  }
+
+  function createStandardEmotions() {
+    Promise.all([
+      emotionsArray.$add({name: 'Angry', type: 'negative'}),
+      emotionsArray.$add({name: 'Sad', type: 'negative'}),
+      emotionsArray.$add({name: 'Jealous', type: 'negative'}),
+      emotionsArray.$add({name: 'Frustrated', type: 'negative'})
+    ]).then(function (ref) {
+      console.log('Standard emotions created');
+    });
+
+  }
+
 });
